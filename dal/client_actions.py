1e5c3063f69e8503529f82ffb44b8a604bf176d1
seminar0533131761 <chani0533131761@gmail.com>
2023-07-25 15:03:09 +0300

    general_crud1

--- dal/client_actions.py
+++ dal/client_actions.py
@@ -2,12 +2,11 @@ from dal.crud_action import get_row_by_condition
 from dal.network_action import get_devices_by_network_id
 
 
-async def get_all_devices_by_client_id(client_id, get_devices_by_network_idgenetwork=None):
-    client_devices=[]
+async def get_all_devices_by_client_id(client_id):
+    client_devices = []
     client_network = await get_all_networks(client_id)
     for network in client_network:
         client_devices.append(await get_devices_by_network_id(network["id"]))
-
 
 
 async def get_all_networks(client_id):
