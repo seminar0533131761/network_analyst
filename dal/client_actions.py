535b447f9a5cf9b856fbc7a12eacddaef12877fc
SariFoyershteen2 <hand1b17@mbjcomp.org.il>
2023-07-25 14:39:17 +0300

    network & clinet actions

--- dal/client_actions.py
+++ dal/client_actions.py
@@ -1,8 +1,27 @@
-
+from connection import connectionObject
+from network_action import get_devices_by_network_id
 async def get_all_devices_by_client_id():
-    pass
+    network_ids = []
+    client_network = get_all_networks()
+    for network in client_network:
+        network_ids.append(network["id"])
+    for i in network_ids:
+        get_all_networks(i)
+
 
 
 async def get_all_networks():
-    pass
+    try:
+        with connectionObject.cursor() as cursor:
+            client_id = 'your_client_id'  # Replace with the network ID you want to filter by
+            sql_query = "SELECT * FROM network WHERE client_id = %s"
+            cursor.execute(sql_query, (client_id,))
+            network = cursor.fetchall()
+            return network
+            connectionObject.commit()
+    except Exception as err:
+        print(err)
+        return False
+    finally:
+        connectionObject.close()
 
--- dal/network_action.py
+++ dal/network_action.py
@@ -1,3 +1,16 @@
+from connection import connectionObject
 
-async def get_devices_by_network_id():
-    pass
+async def get_devices_by_network_id(id):
+    try:
+        with connectionObject.cursor() as cursor:
+            network_id = 'your_network_id'
+            sql_query = "SELECT * FROM devices WHERE network_id = %s"
+            cursor.execute(sql_query, (network_id,))
+            devices = cursor.fetchall()
+            return devices
+            connectionObject.commit()
+    except Exception as err:
+        print(err)
+        return False
+    finally:
+        connectionObject.close()
