0a0e84fd9c9262130fe0b848a8aed8a246698cfe
BatShevaYamin <hand2b13@mbjcomp.org.il>
2023-07-31 17:27:10 +0300

    finish authentication in pcap

--- my_api/my_controllers/cap.py
+++ my_api/my_controllers/cap.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, UploadFile, File, Form
+from fastapi import APIRouter, UploadFile, File, Form, Depends
 
+from dal.user_actions import get_by_user_name
 from my_modules.handle_pcap import handle_file
+from my_modules.module_authentication import User, get_current_active_user
 
 router = APIRouter()
 
@@ -8,12 +10,16 @@ router = APIRouter()
 # only pcap files are expected otherwise many errors can be occurred
 @router.post("/upload")
 async def upload_pcap(file: UploadFile = File(...), client_id: int = Form(...), location: str = Form(...),
-                      user_id: int = Form(...)):
+                      current_user: User = Depends(get_current_active_user)):
     # data is dict
-    network_info = {"location": location, "client_id": client_id, "user_id": user_id}
+    print("name!!!!!!!!!!!!!!!!!!!!!!!!!!!",current_user["user_name"])
+    user_id = await get_by_user_name(current_user["user_name"])
+    # print("user_name ",user_id)
+    network_info = {"location": location, "client_id": client_id, "user_id": user_id["id"]}
     file_content = await file.read()
     # network info is location and the client_id
     is_file_processed_successfully = await handle_file(file_content, network_info)
+    print(f"current user: {current_user['user_name']}")
     if is_file_processed_successfully:
         return {file.filename: "uploaded successfully"}
     return {"sorry": "but the file could not be uploaded"}
